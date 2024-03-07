16baf58d74da7b8291af1de0f27fec64cf8e9cd6
Surendher007 <surendherbabu13@gmail.com>
2024-03-07 19:08:54 +0530

    update

--- alert/alert.js
+++ alert/alert.js
@@ -1,1 +1,8 @@
-alert("are you sure, delete")
+alert("are you sure, delete");
+let i = 0;
+const id = setInterval(function () {
+    console.log(i++);
+    if(i > 10) {
+        clearInterval(id);
+    }
+},1000);
